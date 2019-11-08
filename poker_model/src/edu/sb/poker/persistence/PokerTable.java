10b49dbb53057c870252ea02e9f98eda24490595
The-Anh <s67764@beuth-hochschule.de>
2019-11-08 13:15:23 +0100

    reworked

--- poker_model/src/edu/sb/poker/persistence/PokerTable.java
+++ poker_model/src/edu/sb/poker/persistence/PokerTable.java
@@ -1,22 +1,18 @@
 package edu.sb.poker.persistence;
 
 import java.util.Set;
-
 import javax.json.bind.annotation.JsonbProperty;
-import javax.json.bind.annotation.JsonbTransient;
 import javax.json.bind.annotation.JsonbVisibility;
-import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
-import javax.xml.bind.annotation.XmlType;
-import org.eclipse.persistence.annotations.CacheIndex;
-import edu.sb.poker.util.Copyright;
-import edu.sb.poker.util.HashCodes;
 import edu.sb.poker.util.JsonProtectedPropertyStrategy;
 
 
@@ -142,7 +138,7 @@ public class PokerTable extends BaseEntity {
 
 	@JsonbProperty
 	protected long[] getPlayerReference () {
-		// TODO: implement Mareike
-		return new long[1];
+		// TODO: 
+		return new long[0];
 	}
 }
